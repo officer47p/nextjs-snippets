7b31e9e84245f939eece2dd02e8e341fe644bae2
Parsa Hosseini <officer47p@gmail.com>
2025-09-04 11:26:39 +0330

    Add new button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,31 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
 
   const renderedSnippets = snippets.map((snippet) => (
-    <div key={snippet.id}>
-      <h2>{snippet.title}</h2>
-      <pre>{snippet.code}</pre>
-    </div>
+    <Link
+      key={snippet.id}
+      href={`/snippets/${snippet.id}`}
+      className="block mb-4 p-4 border rounded hover:bg-gray-50"
+    >
+      <div className="flex items-center">
+        <h2>{snippet.title}</h2>
+        <h3 className="ml-auto">View</h3>
+      </div>
+    </Link>
   ));
 
   return (
     <div>
-      <h1>Home Page</h1>
-      <ul>{renderedSnippets}</ul>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href="/snippets/new" className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
     </div>
   );
 }
